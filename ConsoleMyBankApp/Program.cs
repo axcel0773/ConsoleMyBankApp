aaa9fbcf70c386f2ce0e01c015da6b631f9e5254
axcel0773 <yuji-watanabe1@hotmail.com>
2021-10-14 22:33:46 -0300

    construtor, atributos, métodos de acessos e modificadores

--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -6,7 +6,19 @@ namespace ConsoleMyBankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente("Gustavo Yuji R. Watanabe", "000.000.000-00", "Estudante de Desenvolvimento");
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = cliente;
+            conta.agencia = 01;
+            conta.conta = 0001;
+
+
+            Console.WriteLine("Conta corrente criada");
+            Console.WriteLine("Titular: {0}", conta.titular.nome);
+            Console.WriteLine("Agência: {0}", conta.agencia);
+            Console.WriteLine("Conta: {0}", conta.conta);
         }
     }
 }
